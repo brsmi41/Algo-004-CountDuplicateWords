7528116616ff8a0043095a4148ee9670064e8cb4
admin <admin@Briannas-MacBook-Pro.local>
2019-08-05 09:46:34 -0400

    new

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,16 @@
 package hbcu.stay.ready.algor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 
     public String countUniqueWords(String input){
-        return null;
+        Map<String,Integer> duplicates = new HashMap<String,Integer>();
+        for(int count = 0; count <= input.length(); count++){
+            System.out.println(count);
+
+        }
+        return input;
     }
 }
