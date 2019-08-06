793558df8cba171b20753f6c66c3063b21d68bd0
admin <admin@Briannas-MacBook-Pro.local>
2019-08-05 21:44:19 -0400

    final

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,16 +1,45 @@
 package hbcu.stay.ready.algor;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Solution {
 
     public String countUniqueWords(String input){
-        Map<String,Integer> duplicates = new HashMap<String,Integer>();
-        for(int count = 0; count <= input.length(); count++){
-            System.out.println(count);
+       Map<String,Integer> duplicates = new LinkedHashMap<>();
+
+        String result = "";
+        String updatedInput = input.replace(".","");
+        String updateInput = updatedInput.replace("!","");
+        String[] word = updateInput.split(" ");
+        for(String a: word){
+            if(!duplicates.containsKey(a)){
+                duplicates.put(a,1);
+            }
+            else{
+                int currentValue = duplicates.get(a);
+                currentValue++;
+                duplicates.put(a,currentValue);
+            }
+
 
         }
-        return input;
+        result += String.format("The %d unique words are:\n", duplicates.size());
+        int size = 0;
+        for (String s : duplicates.keySet()) {
+            size++;
+            int value = duplicates.get(s);
+            if (size < duplicates.size()) {
+                result += String.format("%s (Seen %d)\n", s, value);
+            }
+            else {
+                result += String.format("%s (Seen %d)", s, value);
+            }
+        }
+        System.out.println(result);
+        return result;
+
     }
 }
